7002a8a4f2a6b382a612841e882526b8dd380965
Cameron Stark <starkc920@outlook.com>
2020-02-12 12:25:59 -0500

    added base packages

---
+++ ecocarhmi/lib/pages/selection_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SelectionPage extends StatefulWidget {
+
+  @override
+  SelectionPageState createState() => SelectionPageState();
+}
+
+class SelectionPageState extends State<SelectionPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      color: Colors.white,
+    );
+  }
+}
